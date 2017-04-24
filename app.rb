82c93825abe013cd4ef1c6174982b67d010a985b
Nigel Said <nigelsaid@gmail.com>
2017-04-24 18:03:32 +0100

    adds params

--- app.rb
+++ app.rb
@@ -17,7 +17,13 @@ get '/wow' do
   "How? What? Why?"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
